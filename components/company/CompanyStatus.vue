8acdee10f83670eec2ed6b3e6a973ec6549d036d
Lau Pei Tong <90409271+LauPeiTong@users.noreply.github.com>
2023-05-07 10:16:51 +0800

    Add

--- components/company/CompanyStatus.vue
+++ components/company/CompanyStatus.vue
@@ -4,9 +4,8 @@
       .company.fill-width.py-4
         p.subtitle-2.px-4.pt-2.mb-0 Prediction
         p.text-h3.px-4.mb-2.pt-0.green--text Investable
-          |
-          span.text-h6.font-weight-regular.secondary--text.ml-2 (85.6%)
-        a.subtitle-1.px-4.mb-0.font-weight-regular.text-decoration-underline View Details
+          ApexCharts.d-flex.justify-space-around(type="donut" :options="chartOptions" :series="series" width="320" height="320")
+        a.subtitle-1.px-4.mb-0.font-weight-regular.d-flex.justify-space-around View Analysis
 
 </template>
 
@@ -15,6 +14,58 @@ export default {
   name: 'CompanyStatus',
   data () {
     return {
+      series: [14.4, 85.6],
+      chartOptions: {
+        chart: {
+          type: 'donut'
+        },
+        legend: {
+          show: false
+        },
+        labels: ['Not Investable', 'Investment'],
+        colors: ['#B8B8D1', '#5B5F97'],
+        plotOptions: {
+          pie: {
+            expandOnClick: true,
+            donut: {
+              labels: {
+                show: true,
+                total: {
+                  label: 'Confidence level',
+                  showAlways: true,
+                  show: true,
+                  color: '#333333',
+                  fontSize: '20px',
+                  fontWeight: '600',
+                  formatter: function (value) {
+                    const t = 85.6
+                    return t + '%'
+                  }
+                },
+                value: {
+                  color: '#FFC145',
+                  fontSize: '28px',
+                  fontWeight: '600'
+                }
+              }
+            }
+          }
+        },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 300
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }],
+        dataLabels: {
+          enabled: false
+        }
+      }
     }
   },
   methods: {
